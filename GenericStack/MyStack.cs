5cb80eec352bdacb9daf5422b276f8e0190e8245
KimKwangHwan <louie1004@naver.com>
2026-03-12 14:24:47 +0900

    Stack

---
+++ GenericStack/MyStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class MyStack<T>
+{
+    private T[] memory;
+    private int _count = 0;
+    public int Count { get { return _count; } }
+    public MyStack(int capacity)
+    {
+        memory = new T[capacity];
+    }
+
+    public void Push(T item)
+    {
+        memory[_count++] = item;
+    }
+    public T Pop()
+    {
+        return memory[--_count]; 
+    }
+    public T Peek()
+    {
+        return memory[_count - 1];
+    }
+    public bool IsEmpty { get { return _count == 0; } }
+}
